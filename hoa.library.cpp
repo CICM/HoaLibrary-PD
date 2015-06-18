c313e3939c229f613b5a9d8b4e7306ad4cfa6122
pierreguillot <guillotpierre6@gmail.com>
2015-06-18 13:38:26 +0200

    hoa + statup message in log

--- hoa.library.cpp
+++ hoa.library.cpp
@@ -13,12 +13,29 @@ char pdversion[] = "Pd-Extended";
 char pdversion[] = "Pd-Vanilla";
 #endif
 
+static t_eclass *cream_class;
+
+static void *hoa_new(t_symbol *s)
+{
+    t_eobj *x = (t_eobj *)eobj_new(cream_class);
+    if(x)
+    {
+        logpost(x, 3, "HOA Library by Julien Colafrancesco, Pierre Guillot, Eliott Paris & Thomas Le Meur\n© 2013 - 2015  CICM | Paris 8 University\nVersion %s (%s) for %s %i.%i\n",hoaversion, __DATE__, pdversion, PD_MAJOR_VERSION, PD_MINOR_VERSION);
+    }
+    return (x);
+}
+
+
 extern "C" void hoa_setup(void)
 {
-    post("HOA Library by Julien Colafrancesco, Pierre Guillot, Eliott Paris & Thomas Le Meur");
-    post("© 2012 - 2015  CICM | Paris 8 University");
-    post("Version %s (%s) for %s.", hoaversion, __DATE__, pdversion);
-    post("");
+    cream_class = eclass_new("hoa", (method)hoa_new, (method)eobj_free, (short)sizeof(t_eobj), CLASS_PD, A_NULL, 0);
+    cream_class = eclass_new("Hoa", (method)hoa_new, (method)eobj_free, (short)sizeof(t_eobj), CLASS_PD, A_NULL, 0);
+    t_eobj* obj = (t_eobj *)hoa_new(NULL);
+    if(!obj)
+    {
+        verbose(3, "HOA Library by Julien Colafrancesco, Pierre Guillot, Eliott Paris & Thomas Le Meur\n© 2013 - 2015  CICM | Paris 8 University\nVersion %s (%s) for %s %i.%i\n",hoaversion, __DATE__, pdversion, PD_MAJOR_VERSION, PD_MINOR_VERSION);
+        eobj_free(obj);
+    }
 
     // HOA COMMON //
     setup_hoa0x2econnect();
